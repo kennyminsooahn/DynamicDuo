d3d32c449362a16f604c83217d2a0f6b85fa93a6
kennyminsooahn <104657371+kennyminsooahn@users.noreply.github.com>
2022-04-29 18:15:46 -0700

    Update Hello.java

--- CSC131 Lab2/src/Hello.java
+++ CSC131 Lab2/src/Hello.java
@@ -1,9 +1,11 @@
+//Kenny Ahn
+//Renay Sharma
 public class Hello {
 public static void main(String[] args) {
 System.out.println("Welcome to Git!");
 System.out.println("yang - This line was added by Dr. Yang.");
 // Add a new line with your information
 System.out.println("301462603 - This line was added by Kenny Ahn.");
-System.out.println("SacStateID - This line was added by First name Last name.");
+System.out.println("301288260 - This line was added by Renay Sharma.");
 }
 }
